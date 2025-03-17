e8d6e46db71bab28c19d561cb6fbde8e552ee522
RijenMaharjan <rijenmhrzn94@gmail.com>
2025-03-17 11:28:44 +0545

    login issue solved

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,7 +1,54 @@
+// import { Link } from "react-router-dom";
+// import { Images } from "../../assets/images";
+// import FAQ from "../../components/screens/home/FAQ/FAQ";
+// import "./Header.scss";
+
+// const Header = () => {
+//   return (
+//     <header className="pg-header">
+//       <div className="container">
+//         <div className="header-wrap">
+//           <Link to="/" className="site-brand">
+//             <img src="src/assets/images/LogoJapan.png" alt="" />
+//           </Link>
+//           <nav className="navbar">
+//             <ul className="nav-menu">
+//               <li className="nav-item">
+//                 <Link to="/">Home</Link>
+//               </li>
+//               {/* <li className="nav-item">
+//                 <Link to="/templates">Templates</Link>
+//               </li> */}
+//               <li className="nav-item">
+//                 <Link to="">FAQ</Link>
+//               </li>
+//               <li className="nav-item">
+//                 <Link to="/about">Contact</Link>
+//               </li>
+//             </ul>
+//             <Link to="/login" className="resume-btn">
+//               Login
+//             </Link>
+//           </nav>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// };
+
+// export default Header;
 import { Link } from "react-router-dom";
 import { Images } from "../../assets/images";
 import FAQ from "../../components/screens/home/FAQ/FAQ";
 import "./Header.scss";
+
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+  SignIn,
+} from "@clerk/clerk-react";
 
 const Header = () => {
   return (
@@ -26,9 +73,15 @@ const Header = () => {
                 <Link to="/about">Contact</Link>
               </li>
             </ul>
-            <Link to="/login" className="resume-btn">
-              Login
-            </Link>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+
+            <SignedOut>
+              <Link to="/login" className="resume-btn">
+                Login
+              </Link>
+            </SignedOut>
           </nav>
         </div>
       </div>
